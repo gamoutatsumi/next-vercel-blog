44d9c216442656dfa5ad81acf8db8540c006c530
gamoutatsumi <47162587+gamoutatsumi@users.noreply.github.com>
2020-11-18 16:22:32 +0900

    fix privacypolicy link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,7 +11,7 @@ const Header: React.FC<Props> = ({ siteName }) => {
       <Link href='/post'>
         <a className='text-md md:text-xl my-auto align-middle px-2 py-3'>{siteName}</a>
       </Link>
-      <Link href='privacypolicy'>
+      <Link href='/privacypolicy'>
         <a className="text-sm md:text-md my-auto hover:bg-gray-300 px-2 duration-300 rounded-md">Privacy Policy</a>
       </Link>
     </header>
