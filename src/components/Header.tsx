be3117204abab7788600e339e2fae6fd2d2404b7
gamoutatsumi <47162587+gamoutatsumi@users.noreply.github.com>
2020-11-18 15:36:47 +0900

    add privacypolicy

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,9 +7,12 @@ interface Props {
 
 const Header: React.FC<Props> = ({ siteName }) => {
   return (
-    <header className='flex shadow-md bg-white'>
+    <header className='flex justify-between shadow-md bg-white'>
       <Link href='/post'>
         <a className='text-xl m-2'>{siteName}</a>
+      </Link>
+      <Link href='privacypolicy'>
+        <a className="text-md m-2 hover:bg-gray-300 p-2 duration-300 rounded-md align-middle">Privacy Policy</a>
       </Link>
     </header>
   )
---
+++ src/pages/privacypolicy.tsx
@@ -0,0 +1,106 @@
+import React, { FunctionComponent } from 'react'
+import Layout from '@/components/Layout'
+import ReactMarkdown from 'react-markdown'
+import toc from 'remark-toc'
+import gfm from 'remark-gfm'
+import GithubSlugger from 'github-slugger'
+
+import { listContentFiles, PostContent, readContentFile } from '@/lib/content-loader'
+import { GetStaticPaths, GetStaticProps, NextPage } from 'next'
+import CodeBlock from '@/components/CodeBlock'
+
+const slugger = new GithubSlugger()
+
+interface MarkdownProps {
+  level?: number
+  children: JSX.Element[]
+  ordered: boolean
+}
+
+const Heading: FunctionComponent<MarkdownProps> = (props) => {
+  if (props.children == null || props.children === undefined) {
+    return React.createElement('')
+  }
+  if (props.level === undefined) {
+    return React.createElement('')
+  }
+  if (props.level === 2) {
+    return React.createElement(
+      `h${props.level}`,
+      {
+        id: slugger.slug(props.children[0].props.children),
+        className: 'my-2 border-b-2'
+      },
+      props.children
+    )
+  }
+  return React.createElement(
+    `h${props.level}`,
+    {
+      id: slugger.slug(props.children[0].props.children),
+      className: 'my-2'
+    },
+    props.children
+  )
+}
+
+const Paragraph: FunctionComponent<MarkdownProps> = (props) => {
+  if (props.children == null || props.children === undefined) {
+    return React.createElement('')
+  }
+  return React.createElement(
+    'p',
+    {
+      className: 'mx-2 my-1'
+    },
+    props.children
+  )
+}
+
+const List: FunctionComponent<MarkdownProps> = (props) => {
+  if (props.children == null || props.children === undefined) {
+    return React.createElement('')
+  }
+  if (props.ordered) {
+    return React.createElement(
+      'ol',
+      {
+        className: 'mx-2 my-1 pl-6 list-decimal'
+      },
+      props.children
+    )
+  } else {
+    return React.createElement(
+      'ul',
+      {
+        className: 'mx-4 my-1 pl-6 list-disc'
+      },
+      props.children
+    )
+  }
+}
+
+const Post: NextPage<PostContent> = ({ title, content, published, image, keyword }) => {
+  return (
+    <Layout title={title} isArticle={true} keyword={keyword} image={image}>
+      <div className="post-meta">
+        <span>{published}</span>
+      </div>
+      <div>
+        <h1>{title}</h1>
+        <ReactMarkdown plugins={[[gfm], [toc, { heading: '目次' }]]} renderers={{ code: CodeBlock, heading: Heading, paragraph: Paragraph, list: List }} source={content} />
+      </div>
+    </Layout>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const content = readContentFile({ slug: 'privacypolicy', isPost: false })
+  return {
+    props: {
+      ...content
+    }
+  }
+}
+
+export default Post
