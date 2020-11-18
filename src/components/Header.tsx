e741883535555d56266d9c706dd1ef3cac30944c
gamoutatsumi <47162587+gamoutatsumi@users.noreply.github.com>
2020-11-18 16:17:10 +0900

    fix style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,10 +9,10 @@ const Header: React.FC<Props> = ({ siteName }) => {
   return (
     <header className='flex justify-between shadow-md bg-white'>
       <Link href='/post'>
-        <a className='text-xl m-2'>{siteName}</a>
+        <a className='text-md md:text-xl my-auto align-middle px-2 py-3'>{siteName}</a>
       </Link>
       <Link href='privacypolicy'>
-        <a className="text-md m-2 hover:bg-gray-300 p-2 duration-300 rounded-md align-middle">Privacy Policy</a>
+        <a className="text-sm md:text-md my-auto hover:bg-gray-300 px-2 duration-300 rounded-md">Privacy Policy</a>
       </Link>
     </header>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,13 +17,13 @@ module.exports = {
     plugin(function ({addBase, config}) {
       addBase({
         'h1': {
-          fontSize: config('theme.fontSize.2xl')
-        },
-        'h2': {
           fontSize: config('theme.fontSize.xl')
         },
-        'h3': {
+        'h2': {
           fontSize: config('theme.fontSize.lg')
+        },
+        'h3': {
+          fontSize: config('theme.fontSize.md')
         }
       })
     })
