ef46c8e826ae9a8f85ca146b9c1caf3f304e29d2
gamoutatsumi <47162587+gamoutatsumi@users.noreply.github.com>
2020-11-19 10:59:06 +0900

    update react and fix fontawesome

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
+import { config, library } from '@fortawesome/fontawesome-svg-core'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faCalendar, faFolder } from '@fortawesome/free-regular-svg-icons'
+import '@fortawesome/fontawesome-svg-core/styles.css'
 import { AppProps } from 'next/app'
 import React, { ReactElement } from 'react'
+
+config.autoAddCss = false
+
+library.add(faFolder, faCalendar, faGithub)
 
 const MyApp = ({ Component, pageProps }: AppProps): ReactElement => (
   <Component {...pageProps} />
