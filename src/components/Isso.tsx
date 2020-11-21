6a612cd918e67a7302e1ab9cab020348f57df028
gamoutatsumi <47162587+gamoutatsumi@users.noreply.github.com>
2020-11-21 14:41:02 +0900

    fix loading isso

--- src/components/Isso.tsx
+++ src/components/Isso.tsx
@@ -1,29 +1,27 @@
-import React, { useEffect } from 'react'
+import React from 'react'
+import Head from 'next/head'
 
 const Isso: React.FC = () => {
-  useEffect(() => {
-    if (window.Isso === undefined) return
-    if (document.getElementById('isso-root') === null) {
-      window.Isso.init()
-    }
-  })
   return (
-    <div className="comment mt-3">
-      <script data-isso="//isso.gamou-tatsumi.com/"
-        data-isso-reply-to-self="true"
-        data-isso-vote="true"
-        data-isso-vote-levels="0,5,10,25,100"
-        data-isso-require-author="false"
-        data-isso-require-email="false"
-        data-isso-css="true"
-        src="//isso.gamou-tatsumi.com/js/embed.min.js"></script>
-      <section id="isso-thread"></section>
-      <noscript>
-        <div>
-          Please enable Javascript to view comments.
-        </div>
-      </noscript>
-    </div>
+    <>
+      <Head>
+        <script async data-isso="https://isso.gamou-tatsumi.com/"
+          data-isso-reply-to-self="true"
+          data-isso-vote="false"
+          data-isso-require-author="false"
+          data-isso-require-email="false"
+          data-isso-css="true"
+          src="https://isso.gamou-tatsumi.com/js/embed.min.js"></script>
+      </Head>
+      <div className="comment mt-3">
+        <section id="isso-thread"></section>
+        <noscript>
+          <div>
+            Please enable Javascript to view comments.
+          </div>
+        </noscript>
+      </div>
+    </>
   )
 }
 
